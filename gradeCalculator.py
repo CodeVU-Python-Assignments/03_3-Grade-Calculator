408919227f480727ff16affaab04cd65fed51fd3
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-01-31 12:19:28 -0600

    Update gradeCalculator.py

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,9 +1,7 @@
 def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
-    # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
 
     # end assignment
 
